b409de84a6fcaf6b57623d268abe74666b2c25d9
Salman Haider <salman.haider@wearenova.co.uk>
2022-10-14 15:39:05 +0500

    added redux and interface

--- src/Actions/WeatherDataAction.tsx
+++ src/Actions/WeatherDataAction.tsx
@@ -10,12 +10,10 @@ export type GetWeatherData = {
 };
 
 export const getWeatherData = () => {
-  debugger;
   return async (dispatch: Dispatch<AnyAction>) => {
     const response = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?appid=d90bb647941b933f55f73738b69a7c4e&lat=33.6938118&lon=73.0651511"
     );
-    debugger;
     const responseJSON = await response.json();
     return dispatch({
       type: GET_WEATHER_DATA,
